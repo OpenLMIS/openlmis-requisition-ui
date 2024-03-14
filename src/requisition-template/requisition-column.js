0cd909dbd443d79a34f5cd0685804597414b6a09
mdulko <mdulko@soldevelo.com>
2024-03-14 12:48:30 +0100

    OLMIS-7899: code improvement

--- src/requisition-template/requisition-column.js
+++ src/requisition-template/requisition-column.js
@@ -189,8 +189,10 @@
             }
 
             var approvalColumns = requisition.template.patientsTabEnabled ?
-                [TEMPLATE_COLUMNS.TOTAL_RECEIVED_QUANTITY, TEMPLATE_COLUMNS.REMARKS] :
-                [TEMPLATE_COLUMNS.APPROVED_QUANTITY, TEMPLATE_COLUMNS.REMARKS];
+                [TEMPLATE_COLUMNS.TOTAL_RECEIVED_QUANTITY] :
+                [TEMPLATE_COLUMNS.APPROVED_QUANTITY];
+
+            approvalColumns.push(TEMPLATE_COLUMNS.REMARKS);
 
             return column.isDisplayed && (
                 approvalColumns.indexOf(column.name) === -1 ||
